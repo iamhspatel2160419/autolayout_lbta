38d14307fcf905dc4de6633c732b16309244fd0a
Brian Voong <bhlvoong@gmail.com>
2017-10-13 20:34:49 -0700

    Ep 5 MVC

--- autolayout_lbta/PageCell.swift
+++ autolayout_lbta/PageCell.swift
@@ -10,14 +10,31 @@ import UIKit
 
 class PageCell: UICollectionViewCell {
     
-    let bearImageView: UIImageView = {
+    var page: Page? {
+        didSet {
+//            print(page?.imageName)
+            
+            guard let unwrappedPage = page else { return }
+            
+            bearImageView.image = UIImage(named: unwrappedPage.imageName)
+            
+            let attributedText = NSMutableAttributedString(string: unwrappedPage.headerText, attributes: [NSAttributedStringKey.font: UIFont.boldSystemFont(ofSize: 18)])
+            
+            attributedText.append(NSAttributedString(string: "\n\n\n\(unwrappedPage.bodyText)", attributes: [NSAttributedStringKey.font: UIFont.systemFont(ofSize: 13), NSAttributedStringKey.foregroundColor: UIColor.gray]))
+            
+            descriptionTextView.attributedText = attributedText
+            descriptionTextView.textAlignment = .center
+        }
+    }
+    
+    private let bearImageView: UIImageView = {
         let imageView = UIImageView(image: #imageLiteral(resourceName: "bear_first"))
         imageView.translatesAutoresizingMaskIntoConstraints = false
         imageView.contentMode = .scaleAspectFit
         return imageView
     }()
     
-    let descriptionTextView: UITextView = {
+    private let descriptionTextView: UITextView = {
         let textView = UITextView()
         
         let attributedText = NSMutableAttributedString(string: "Join us today in our fun and games!", attributes: [NSAttributedStringKey.font: UIFont.boldSystemFont(ofSize: 18)])
--- autolayout_lbta/SwipingController.swift
+++ autolayout_lbta/SwipingController.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class SwipingController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
     
+    let pages = [
+        Page(imageName: "bear_first", headerText: "Join use today in our fun and games!", bodyText: "Are you ready for loads and loads of fun? Don't wait any longer! We hope to see you in our stores soon."),
+        Page(imageName: "heart_second", headerText: "Subscribe and get coupons on our daily events", bodyText: "Get notified of the savings immediately when we announce them on our website. Make sure to also give us any feedback you have."),
+        Page(imageName: "leaf_third", headerText: "VIP members special services", bodyText: "")
+    ]
+    
+//    let imageNames = ["bear_first", "heart_second", "leaf_third"]
+//    let headerStrings = ["Join use today in our fun and games!", "Subscribe and get coupons on our daily events", "VIP members special services"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -24,11 +33,22 @@ class SwipingController: UICollectionViewController, UICollectionViewDelegateFlo
     }
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 4
+        return pages.count
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellId", for: indexPath)
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellId", for: indexPath) as! PageCell
+        
+        let page = pages[indexPath.item]
+        cell.page = page
+        
+        
+//        cell.bearImageView.image = UIImage(named: page.imageName)
+//        cell.descriptionTextView.text = page.headerText
+        
+//        let imageName = imageNames[indexPath.item]
+//        cell.bearImageView.image = UIImage(named: imageName)
+//        cell.descriptionTextView.text = headerStrings[indexPath.item]
 
         // definitely don't try this, it is a very bad idea
 //        let imageView = UIImageView()
